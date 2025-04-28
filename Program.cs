f8146a22edcdb74a768739c3bbe4ec54d9f2bcf0
RodrigoMartinez <rodrigomartinezdev@outlook.com>
2025-04-27 22:57:35 -0600

    Finaliza primera parte del proyecto. Traiga los Pushed y los Opened Issues

--- Program.cs
+++ Program.cs
@@ -1,10 +1,32 @@
-﻿using System.Net.Http;
-using System.Text.Json;
-using gitHub_user_activity.CLI;
-using gitHub_user_activity.Models;
-using Newtonsoft.Json;
+﻿using gitHub_user_activity.CLI;
+using System.CommandLine;
+using System.CommandLine.Invocation;
 
 var manager = new GitMannager();
 
-await manager.GetCountPushedCommits("Rodrigotmz");
-await manager.GetIssues("Rodrigotmz");
+var addCommandGit = new Argument<string>("username", "The username to fetch GitHub Activity");
+
+addCommandGit.AddValidator((result) =>
+{
+    if (string.IsNullOrWhiteSpace(result.GetValueForArgument(addCommandGit)))
+    {
+        result.ErrorMessage = "Please provide a valid username.";
+    }
+});
+
+
+var rootCommand = new RootCommand
+{
+    addCommandGit
+};
+
+rootCommand.SetHandler(async (username) =>
+{
+    await manager.GetCountPushedCommits(username);
+    await manager.GetIssues(username);
+}, addCommandGit);
+
+//await manager.GetCountPushedCommits("Rodrigotmz");
+//await manager.GetIssues("Rodrigotmz");
+string[] arg = ["Rodrigotmz"];
+await rootCommand.InvokeAsync(arg);
